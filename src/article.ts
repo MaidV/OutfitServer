df4b6b898fc251951faf1d3781804ea4f9098f33
Robert Blackwell <robert.blackwell@protonmail.com>
2020-12-24 14:33:00 -0500

    Change to 4 space indent, ffs

--- src/article.ts
+++ src/article.ts
@@ -1,37 +1,37 @@
 
 export class Article {
-  public readonly name: string = "";
-  public readonly FormID: string = "";
-  public readonly EDID: string = "";
-  public readonly slots: Array<string> = [];
+    public readonly name: string = "";
+    public readonly FormID: string = "";
+    public readonly EDID: string = "";
+    public readonly slots: Array<string> = [];
 
-  constructor(form: {
-    "FULL - Name": string,
-    "Record Header": { "FormID": string, "Record Flags": { "Non-Playable": string } },
-    "EDID - Editor ID": string,
-    "BOD2 - Biped Body Template": { "First Person Flags": any },
-  }) {
-    try {
-      if (form["Record Header"]["Record Flags"]["Non-Playable"])
-        throw 'Non-playable form';
-      this.name = form["FULL - Name"];
-      if (!this.name)
-        throw 'Undefined name';
-      this.FormID = "0x" + form["Record Header"]["FormID"].substr(2);
-      this.EDID = form["EDID - Editor ID"];
-      this.slots = Object.keys(form["BOD2 - Biped Body Template"]["First Person Flags"]);
-      if (Object.keys(this.slots).length == 0)
-        throw 'No slots defined';
+    constructor(form: {
+        "FULL - Name": string,
+        "Record Header": { "FormID": string, "Record Flags": { "Non-Playable": string } },
+        "EDID - Editor ID": string,
+        "BOD2 - Biped Body Template": { "First Person Flags": any },
+    }) {
+        try {
+            if (form["Record Header"]["Record Flags"]["Non-Playable"])
+                throw 'Non-playable form';
+            this.name = form["FULL - Name"];
+            if (!this.name)
+                throw 'Undefined name';
+            this.FormID = "0x" + form["Record Header"]["FormID"].substr(2);
+            this.EDID = form["EDID - Editor ID"];
+            this.slots = Object.keys(form["BOD2 - Biped Body Template"]["First Person Flags"]);
+            if (Object.keys(this.slots).length == 0)
+                throw 'No slots defined';
+        }
+        catch (e) {
+            throw e;
+        }
     }
-    catch (e) {
-      throw e;
-    }
-  }
 
-  public Draw(): HTMLElement {
-    let divEl = document.createElement("div");
-    divEl.innerHTML = `${this.name}<br>&nbsp&nbsp&nbsp&nbsp${this.slots.join(', ')}`;
-    return divEl;
-  }
+    public Draw(): HTMLElement {
+        let divEl = document.createElement("div");
+        divEl.innerHTML = `${this.name}<br>&nbsp&nbsp&nbsp&nbsp${this.slots.join(', ')}`;
+        return divEl;
+    }
 
 }
--- src/index.ts
+++ src/index.ts
@@ -1,93 +1,93 @@
 import { Article } from './article'
 
 function parse(file: File) {
-  return new Promise((resolve, reject) => {
-    const reader: FileReader = new FileReader();
+    return new Promise((resolve, reject) => {
+        const reader: FileReader = new FileReader();
 
-    reader.onload = function(e: Event) {
-      if (!this.result || this.result instanceof ArrayBuffer)
-        return;
-      resolve(JSON.parse(this.result));
-    };
-    reader.onerror = function(e: any) {
-      reject(e);
-    };
-    reader.readAsText(file);
-  });
+        reader.onload = function(e: Event) {
+            if (!this.result || this.result instanceof ArrayBuffer)
+                return;
+            resolve(JSON.parse(this.result));
+        };
+        reader.onerror = function(e: any) {
+            reject(e);
+        };
+        reader.readAsText(file);
+    });
 };
 
 function updateFoldables() {
-  let coll = document.getElementsByClassName("collapsible");
-  for (let i = 0; i < coll.length; i++) {
-    coll[i].addEventListener("click", function() {
-      this.classList.toggle("active");
-      var content = this.nextElementSibling;
-      if (content.style.display === "block") {
-        content.style.display = "none";
-      } else {
-        content.style.display = "block";
-      }
-    });
-  }
+    let coll = document.getElementsByClassName("collapsible");
+    for (let i = 0; i < coll.length; i++) {
+        coll[i].addEventListener("click", function() {
+            this.classList.toggle("active");
+            var content = this.nextElementSibling;
+            if (content.style.display === "block") {
+                content.style.display = "none";
+            } else {
+                content.style.display = "block";
+            }
+        });
+    }
 }
 
-let fileSelector = document.getElementById("file-selector");
-
 let articles = new Map<string, Array<Article>>();
-fileSelector?.addEventListener("change",
-  async function loadJSONFiles(event: Event) {
+async function loadJSONFiles(event: Event) {
     const target = event.target as HTMLInputElement;
     const files = target.files;
 
     if (!files)
-      return;
+        return;
 
     let inputObjs = new Map<string, any>();
     for (let f of files) {
-      let key = f.name.replace(/\.json/, "");
-      let formMap = await parse(f);
-      inputObjs.set(key, formMap);
+        let key = f.name.replace(/\.json/, "");
+        let formMap = await parse(f);
+        inputObjs.set(key, formMap);
     };
-
 
     articles.clear();
     for (let [mod, rawforms] of inputObjs.entries()) {
-      let localArticles = Array<Article>();
-      for (let formid in rawforms) {
-        try {
-          let newArticle = new Article(rawforms[formid]);
-          localArticles.push(newArticle);
+        let localArticles = Array<Article>();
+
+        for (let formid in rawforms) {
+            try {
+                let newArticle = new Article(rawforms[formid]);
+                localArticles.push(newArticle);
+            }
+            catch (e) {
+                console.log("Record not a valid armor piece: ", e,
+                    rawforms[formid]);
+            }
         }
-        catch (e) {
-          console.log("Record not a valid armor piece: ", e,
-            rawforms[formid]);
-        }
-      }
-      articles.set(mod, localArticles);
+        articles.set(mod, localArticles);
     }
 
     let modContainer = document.getElementById("articleContainer");
     if (!modContainer)
-      return;
+        return;
 
     modContainer.innerHTML = "";
     for (let [mod, local_articles] of articles.entries()) {
-      console.log(mod, local_articles);
-      let modBtn = document.createElement("BUTTON");
-      modBtn.textContent = mod;
-      modBtn.className = "collapsible";
-      modContainer.appendChild(modBtn);
-      let articleContainer = document.createElement("div");
-      articleContainer.className = "content";
-      for (let article of local_articles) {
-        let articleDiv = article.Draw();
-        articleDiv.draggable = true;
-        articleContainer.appendChild(articleDiv);
-      }
-      articleContainer.style.display = "none";
-      modContainer.appendChild(articleContainer);
+        console.log(mod, local_articles);
+        let modBtn = document.createElement("BUTTON");
+        modBtn.textContent = mod;
+        modBtn.className = "collapsible";
+        modContainer.appendChild(modBtn);
+        let articleContainer = document.createElement("div");
+        articleContainer.className = "content";
+        for (let article of local_articles) {
+            let articleDiv = article.Draw();
+            articleDiv.draggable = true;
+            articleContainer.appendChild(articleDiv);
+        }
+        articleContainer.style.display = "none";
+        modContainer.appendChild(articleContainer);
     }
 
     updateFoldables();
 
-  });
+}
+
+let fileSelector = document.getElementById("file-selector");
+fileSelector?.addEventListener("change", loadJSONFiles);
