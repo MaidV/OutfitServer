563b9ad04039822f6dc152bc4c6e4ee562871d34
MasterDev <MasterDev@dummy.com>
2021-02-16 07:19:02 -0500

    Add equip functionality from web request :)

--- src/outfit_server.cpp
+++ src/outfit_server.cpp
@@ -85,8 +85,8 @@ namespace ArticleNS
 		Article(){};
 
 		Article(TESObjectARMO* armor) :
-			name(armor->GetFullName()), 
-			editorID(armor->GetFormEditorID()), 
+			name(armor->GetFullName()),
+			editorID(armor->GetFormEditorID()),
 			formID(mask_form<int32_t, 6>(armor)),
 			form(armor){};
 	};
@@ -94,15 +94,15 @@ namespace ArticleNS
 	void to_json(json& j, const Article& a);
 
 	typedef unordered_map<string, unordered_map<string, ArticleNS::Article>> armor_record_t;
-	armor_record_t armorRecords;
+	armor_record_t armor_map;
 
 	void LoadArmors()
 	{
-		if (!armorRecords.empty())
+		if (!armor_map.empty())
 			return;
 
 		TESDataHandler* dataHandler = TESDataHandler::GetSingleton();
-		armorRecords.clear();
+		armor_map.clear();
 		logger::info("Loading armor from mods");
 		bool ignoreSkin = true;
 		bool playable = true;
@@ -171,53 +171,22 @@ namespace ArticleNS
 
 			ArticleNS::Article article(armor);
 			logger::debug("Created new Article: " + json(article).dump());
-			armorRecords[armor->GetFile()->fileName][mask_form<string, 6>(article.form)] = article;
+			armor_map[armor->GetFile()->fileName][mask_form<string, 6>(article.form)] = article;
 		}
-		logger::info("dummy");
 
 		logger::info("Loaded all armors into Armor Map");
 		std::ofstream out;
 		out.open("YEAHHHHH.json");
 		logger::info("Opened dump file");
-		out << json(armorRecords).dump(4);
+		out << json(armor_map).dump(4);
 		logger::info("Finished dump");
 	}
 
 	armor_record_t& GetLoadedArmors()
 	{
-		LoadArmors();
-		return ArticleNS::armorRecords;
+		return ArticleNS::armor_map;
 	};
 
-	void from_json(const json& j, Article& a)
-	{
-		LoadArmors();
-		string mod = j.at("mod");
-		string formID = j.at("formID");
-		a = Article(armorRecords[mod][formID]);
-	}
-
-	void to_json(json& j, const Article& a)
-	{
-		j = json{
-			{ "name", a.name },
-			{ "formID", mask_form<string, 6>(a.form) },
-			{ "editorID", a.editorID },
-			{ "slots", a.slots }
-		};
-	}
-}
-
-namespace OutfitNS
-{
-	struct Outfit
-	{
-	};
-}
-
-
-namespace SluttifyArmor
-{
 	void AddItem(Actor* actor, std::vector<TESForm*>& forms, int32_t count = 1, bool silent = false)
 	{
 		const auto scriptFactory = RE::IFormFactory::GetConcreteFormFactoryByType<RE::Script>();
@@ -235,27 +204,67 @@ namespace SluttifyArmor
 		}
 	}
 
-	void TryOutfit(Actor* actor)
+	void from_json(const json& j, Article& a)
 	{
-		auto& armors = ArticleNS::GetLoadedArmors();
-		std::vector<TESForm*> toEquip{
-			armors["Skyrim.esm"]["01B3A3"].form,
-			armors["Skyrim.esm"]["012E46"].form
-		};
-		AddItem(actor, toEquip, 1);
+		string mod = j.at("mod");
+		string formID = j.at("formID");
+		logger::info("Article: " + mod + " " + formID);
+		a = Article(armor_map[mod][formID]);
+	}
 
+	void to_json(json& j, const Article& a)
+	{
+		j = json{
+			{ "name", a.name },
+			{ "formID", mask_form<string, 6>(a.form) },
+			{ "editorID", a.editorID },
+			{ "slots", a.slots }
+		};
+	}
+}
+
+namespace OutfitNS
+{
+	struct Outfit
+	{
+		string name;
+		std::vector<ArticleNS::Article> articles;
+	};
+	std::unordered_map<string, Outfit> outfit_map;
+
+	void TryOutfit(Actor* actor, OutfitNS::Outfit& outfit)
+	{
 		ActorEquipManager* equipManager = ActorEquipManager::GetSingleton();
+
+		std::vector<TESForm*> toEquip;
+		toEquip.reserve(outfit.articles.size());
+		for (ArticleNS::Article& article : outfit.articles) {
+			toEquip.push_back(article.form);
+		}
+		ArticleNS::AddItem(actor, toEquip, 1);
+
 		for (auto& armor : toEquip)
 			equipManager->EquipObject(actor, static_cast<TESObjectARMO*>(armor), nullptr, 1);
 	}
 
-	//bool RegisterFuncs(VMClassRegistry* a_registry)
-	//{
-	//	a_registry->RegisterFunction(new NativeFunction1<StaticFunctionTag, VMResultArray<TESForm*>, Actor*>("GetOutfit", "SA_Library", SluttifyArmor::GetOutfit, a_registry));
+	void from_json(const json& j, Outfit& o)
+	{
+		logger::info(j.dump());
+		logger::info(j.at("name"));
+		j.at("name").get_to(o.name);
+		j.at("articles").get_to(o.articles);
+		outfit_map[o.name] = o;
+	}
 
-	//	return true;
-	//}
+	void to_json(json& j, const Outfit& o)
+	{
+		j = json{
+			{ "name", o.name },
+			{ "articles", o.articles }
+		};
+	}
 }
+
 
 static const char* s_web_root_dir = "data/outfitmanager";
 static const char* s_listening_address = "http://localhost:8000";
@@ -264,22 +273,41 @@ static void cb(struct mg_connection* c, int ev, void* ev_data, void*)
 {
 	if (ev == MG_EV_HTTP_MSG) {
 		struct mg_http_message* hm = static_cast<mg_http_message*>(ev_data);
+		ArticleNS::LoadArmors();
 
-		if (mg_vcmp(&hm->uri, "/UpdateOutfit") == 0) {
-			logger::info("UpdateOutfit Request received");
+		if (mg_vcmp(&hm->uri, "/LoadArmorData") == 0) {
+			logger::info("LoadArmorData Request received");
 			auto& armors = ArticleNS::GetLoadedArmors();
-			SluttifyArmor::TryOutfit(PlayerCharacter::GetSingleton());
 			mg_http_reply(c,
 				200,
-				"Content-Type: application/json; charset=utf-8\r\nAccess-Control-Allow-Origin: http://localhost:1234\r\n",
+				"Content-Type: application/json; charset=utf-8\r\nAccess-Control-Allow-Origin: *\r\n",
 				json(armors).dump().c_str());
 
+			return;
+		} else if (mg_vcmp(&hm->uri, "/TryOutfit") == 0) {
+			logger::info("TryOutfit Request received");
+			const char* data = static_cast<const char*>(hm->body.ptr);
+			const size_t len = hm->body.len;
+			string outfit_str;
+			outfit_str.reserve(len + 1);
+			for (int i = 0; i < len; ++i)
+				outfit_str[i] = data[i];
+			outfit_str[len] = '\0';
+			logger::info(outfit_str.c_str());
+
+			try {
+				OutfitNS::Outfit outfit = json::parse(outfit_str.c_str());
+				OutfitNS::TryOutfit(PlayerCharacter::GetSingleton(), outfit);
+			} catch (...) {
+				logger::info("Failed to init Outfit from json.");
+			}
+
+			mg_http_reply(c, 200, "Access-Control-Allow-Origin: *\r\n", "");
 			return;
 		}
 
 		struct mg_http_serve_opts opts = { s_web_root_dir, NULL };
 		mg_http_serve_dir(c, hm, &opts);
-		//mg_http_serve_file(c, hm, (web_root + string(hm->uri)).c_str(), "text/html", "");
 	}
 }
 
